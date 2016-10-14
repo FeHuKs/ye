7dc8cd4c84b566f7e69c9f28338833749001ed53
FeHuKs <mixa03@inbox.lv>
2016-10-14 22:49:56 +0300

    2

--- Main.java
+++ Main.java
@@ -1,19 +1,77 @@
-import com.oracle.webservices.internal.api.message.PropertySet;
-import com.sun.deploy.util.Property;
-import com.sun.xml.internal.fastinfoset.sax.Properties;
-
-import java.io.File;
-import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * Created by dom on 13.10.2016.
  */
-public class Main {
+public class Main implements MathM{
 
     public static void main(String[] args) {
+        int number1, number2, number3;
+        char symbol;
 
-        
+
+
+
+        System.out.println("1.) number1 :");
+        Scanner scanner = new Scanner(System.in);
+        number1 = scanner.nextInt();
+        System.out.println("2.) symbols + , * , - , / . :");
+        symbol = scanner.next().charAt(0);
+        System.out.println("3.) number3 :");
+        number2 = scanner.nextInt();
+
+        switch (symbol) {
+            case '+': {}
+            case '-': {}
+            case '*': {}
+            case '/': {}
+            default: {}
+        }
+    }
+
+
+    @Override
+    public void setmU(int number1,int number2) {
 
     }
 
+    @Override
+    public void setmD(int number1,int number2) {
+
+    }
+
+    @Override
+    public void setmV(int number1,int number2) {
+
+    }
+
+    @Override
+    public void setmS(int number1,int number2) {
+
+    }
+
+    @Override
+    public int getmU(int number3) {
+        return number3;
+    }
+
+    @Override
+    public int getmD(int number3) {
+        return number3;
+    }
+
+    @Override
+    public int getmV(int number3) {
+        return number3;
+    }
+
+    @Override
+    public int getmS(int number3) {
+        return number3;
+    }
+
+    @Override
+    public String getError() {
+        return null;
+    }
 }
---
+++ MathM.java
@@ -0,0 +1,17 @@
+/**
+ * Created by dom on 14.10.2016.
+ */
+public interface MathM {
+
+    public void setmU(int number1,int number2);
+    public void setmD(int number1,int number2);
+    public void setmV(int number1,int number2);
+    public void setmS(int number1,int number2);
+
+    public int getmU(int number3);
+    public int getmD(int number3);
+    public int getmV(int number3);
+    public int getmS(int number3);
+    public String getError();
+
+}
