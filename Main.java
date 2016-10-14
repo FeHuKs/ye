0af2714ce60e13a4c65c78eaca64c3c5a1c95910
FeHuKs <mixa03@inbox.lv>
2016-10-14 20:10:29 +0300

    1

---
+++ Main.java
@@ -0,0 +1,19 @@
+import com.oracle.webservices.internal.api.message.PropertySet;
+import com.sun.deploy.util.Property;
+import com.sun.xml.internal.fastinfoset.sax.Properties;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by dom on 13.10.2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        
+
+    }
+
+}
